dca255e74a7b557540848637fda07de8762a7f3b
Carolyn-Louis <carolyn.louis05@gmail.com>
2023-10-07 01:25:41 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("Adding all ML related code here")
+
+def model1():
+    print("Adding code for ml model1")
+
+def model2():
+     print("Adding code for ml model2")
+
+def model3():
+     print("Adding code for ml model3")
